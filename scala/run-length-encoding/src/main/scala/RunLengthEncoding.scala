a064077dc9b1ddfe3249098aeb20685190e0d2e5
Paco Benitez <f.benitez@building-blocks.com>
2017-03-26 09:38:17 +0200

    Some Refactor

--- scala/run-length-encoding/src/main/scala/RunLengthEncoding.scala
+++ scala/run-length-encoding/src/main/scala/RunLengthEncoding.scala
@@ -3,17 +3,16 @@ object RunLengthEncoding {
 
 
   def encode(str: String): String = {
-    var number = 0
     str.foldRight("") {(elem, res) =>
 
-      val headAsNumber = res.takeWhile(_.isDigit)
-      val restExceptNumber = res.dropWhile(_.isDigit)
+      val in = res.takeWhile(_.isDigit)
+      val rest = res.dropWhile(_.isDigit)
 
       if (res.isEmpty) "" + elem
       else if (res.size == 1 && elem == res.head) "2" + elem
       else if (res.size == 1 && elem != res.head) elem + res
-      else if (!headAsNumber.isEmpty && restExceptNumber.head == elem) "" + (headAsNumber.toInt + 1) + restExceptNumber
-      else if (headAsNumber.isEmpty && restExceptNumber.head == elem) "" + 2 + restExceptNumber.head + restExceptNumber.tail
+      else if (!in.isEmpty && rest.head == elem) (in.toInt + 1).toString + rest
+      else if (in.isEmpty && rest.head == elem) "2" + rest.head + rest.tail
       else elem + res
     }
   }
