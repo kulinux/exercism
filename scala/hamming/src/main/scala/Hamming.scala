ffabcde189489c6bc361f2b3586940e3f5ece734
Paco Benitez <f.benitez@building-blocks.com>
2017-03-27 17:57:47 +0200

    Hamming

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,11 +1,11 @@
 
 
 object Hamming {
-  def compute(c1: String, c2: String) : Some[Int] = 
-    if (c1.size != c2.size) None[Int]
-
-    if (c1.isEmpty) Some(0)
-    else if (c1.head == c1.head) compute(c1.tail, c2.tail)
-    else if (c1.head != c1.head) 1 + compute(c1.tail, c2.tail)
+  def compute(c1: String, c2: String) : Option[Int] = {
+    if (c1.size != c2.size) None
+    else if (c1.isEmpty) Some(0)
+    else if (c1.head == c2.head) compute(c1.tail, c2.tail)
+    else if (c1.head != c2.head) Some(1 + compute(c1.tail, c2.tail).getOrElse(0))
     else None
+  }
 }
