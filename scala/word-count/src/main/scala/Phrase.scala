537b1848ab5e285a7f678c53bd774d8bfec1eebf
Paco Benitez <f.benitez@building-blocks.com>
2017-04-07 22:13:48 +0200

    Finished WordCount

--- scala/word-count/src/main/scala/Phrase.scala
+++ scala/word-count/src/main/scala/Phrase.scala
@@ -1,21 +1,16 @@
 
 
 class Phrase(ph : String) {
-  def wordCount2 : Map[String, Int] = {
-    val tokens = ph.split(' ')
+  def filter(a : Seq[String]) = a.map( x =>
+    x.filter(y => y.isLetter || y.isDigit || y == ''')
+  ).filter(!_.isEmpty).map(_.toLowerCase)
+
+  def wordCount : Map[String, Int] = {
+    val tokens = filter(ph.split(" |,"))
+
+
     tokens.map( x =>
       (x -> tokens.filter( _ == x ).size)
     ) toMap
   }
-
-  def wordCount3(ph : Seq[String], current : Map[String, Int]) : Map[String, Int] = {
-    ph match {
-      case Nil => Map()
-      case head :: tail if (current.contains(head)) =>  current - head + (head -> (current(head) + 1))
-      case head :: tail if (!current.contains(head)) => current + (head -> 1)
-      case _ => ???
-    }
-  }
-
-  def wordCount = wordCount3(ph.split(" "), Map())
 }
