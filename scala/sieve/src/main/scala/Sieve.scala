356058e29db401cd734102f275a13bbfeae886db
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2018-06-26 22:52:44 +0200

    Works

--- scala/sieve/src/main/scala/Sieve.scala
+++ scala/sieve/src/main/scala/Sieve.scala
@@ -9,8 +9,10 @@ object NotPrime extends Mark
 object Sieve {
 
   def mark(primes: Seq[(Int, Mark)], chosen: Int) : Seq[(Int, Mark)] = {
-      primes.
-        map{
+      primes
+      .map{
+          case (i, mark) if (mark != MaybePrime)  => (i, mark)
+          case (i, mark) if (i == chosen)  => (i, Prime)
           case (i, mark) if (i % chosen == 0)  => (i, NotPrime)
           case (i, mark) if(i % chosen != 0)  => (i, MaybePrime)
         }
@@ -21,14 +23,23 @@ object Sieve {
       .find( _._2 == MaybePrime )
       .map( _._1 )
 
-    val nextSeq = mark(primes, maybePrime.get)
-    nextSeq
-
+    if(maybePrime.isDefined) {
+      val nextSeq = mark(primes, maybePrime.get)
+      passOne(nextSeq)
+    } else {
+      primes
+    }
   }
 
   def primes(limit: Int) : List[Int] = {
     val range : Seq[Int] = 2 to limit toSeq
-    val a: List[Int] = range.toList
-    a
+    val a = range.map((_, MaybePrime))
+    val res = passOne(a)
+
+    res
+      .filter(_._2 == Prime)
+      .map(_._1)
+      .toList
+
   }
 }
